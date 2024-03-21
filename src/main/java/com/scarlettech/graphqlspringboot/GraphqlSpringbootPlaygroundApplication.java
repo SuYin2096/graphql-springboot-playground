d2ac6e33db950f28d522d25bf8e1ab8d54657778
IT-Wizard\SuYin <suyin2096@gmail.com>
2024-03-21 09:33:49 +0630

    initial commit

---
+++ src/main/java/com/scarlettech/graphqlspringboot/GraphqlSpringbootPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.scarlettech.graphqlspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringbootPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlSpringbootPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scarlettech/graphqlspringboot/GraphqlSpringbootPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scarlettech.graphqlspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSpringbootPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
